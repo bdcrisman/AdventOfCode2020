bdcc7896ae234d5d78c5215aaf8fd5df696df33d
Ben Crisman <bencrisman@bcsquared.tech>
2020-12-18 21:41:30 -0700

    finished part 1

---
+++ Day6/PartOne.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
+{
+    public class PartOne
+    {
+        public int SumYesAnswersFromGroups(string input)
+        {
+            var lines = input.Split(Environment.NewLine).ToList();
+            var l = new List<string>();
+            int sum = 0;
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    sum += SumAnswers(l);
+                    l.Clear();
+                    continue;
+                }
+                l.Add(line.Trim());
+            }
+
+            if (l.Count > 0)
+                sum += SumAnswers(l);
+
+            return sum;
+        }
+
+        private int SumAnswers(List<string> lines)
+        {
+            if (lines == null || lines.Count <= 0)
+                return 0;
+
+            return string.Join("", lines).Distinct().Count();
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            Console.WriteLine($"Part 1: Sum of yes answers from groups: {new PartOne().SumYesAnswersFromGroups(input)}");
+        }
+    }
+}
